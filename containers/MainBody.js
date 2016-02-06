f447bb1ac6b03bed45ed2de79ebf3de8e37d536c
sloppylopez <sloppy@sloppylopez.com>
2016-02-06 18:01:56 +0000

    finished article

--- containers/MainBody.js
+++ containers/MainBody.js
@@ -13,87 +13,81 @@ class MainBody extends Component {
             <div className='container'>
                 <article>
                     <div>
-                        <h2>Bye Bye London, Hello Amsterdam</h2>
+                        <h2>Good Bye London, Hello Amsterdam</h2>
 
                         <div>
                             <p>
                                 A big opportunity
-                                came to my life, I changed job, house and city all at the same time!,
-                                it wasn't really easy because I moved alone, even though my girl
-                                came a month after.
-                            </p>
-                            <p>
+                                came to my life, I changed job, house and city all at the same time,
+                                it wasn't easy because I moved alone, even though my girl
+                                came a month after and helped my out.
                                 I have very good memories of London for the last 3 years, it's a marvelous
-                                city to live but I fell in love with Amsterdam long time ago when I came
-                                with friends.<br></br>
-                                I promised myself one day I will work here somehow, it took me more than 7
-                                years programming to reach being offered a job here.
+                                city to live but I always wanted to live in Amsterdam ever since I came the
+                                first time. It took me more than 7 years programming to reach being offered
+                                a job here.
                             </p>
                             <p>
-                                The first 3 weeks were rought though, It was challenging to find a house
+                                The first 3 weeks were hard knock life, It was challenging to find a house
                                 to rent, <a href='https://www.airbnb.com/'>Airbnb</a> saved my life, but
                                 I was forced to roam from house to house every 2-3 days, it wasn't funny,
                                 I recall sleeping in the bed with the puffy jacket chilling to the bone
                                 because the host did not switch the heating on.
                             </p>
                             <p>
-                                Fortunately I found a very small and pricey flat but off course that wasn't the
-                                end of the problems.<br></br>
-                                It's a first floor and they were building a 'restaurant' in the down floor,
-                                so I got 2 months of nice drilling included in the price, thanks God I was going
-                                to work and the noise was avoided half of the day, my girlfriend wasn't that lucky.
+                                Fortunately I found a very small and pricey flat but that wasn't the end of
+                                the problems off course.
+                                It's a first floor flat and they were building a 'Restaurant' in the down floor,
+                                so we had to suffer 2 months of drilling included in the price, thanks God I was going
+                                to work to scape the noise most of the day, my girlfriend wasn't that lucky.
                             </p>
                             <p>
-                                Nobody said it will be easy, I did not have internet at home at all the first 2 months,
-                                imagine a web developer without internet in a new team.<br></br>
+                                Nobody said it will be easy, I didn't have internet at home at all the first 2 months,
+                                imagine a Web Developer without internet connection in a new team with new technologies
+                                trying to catch up with your colleagues.
                                 Finally after all the problems I have a place to rest and work, so I decided
-                                to continue to work where I left it.
+                                to continue working where I left it before leaving.
                             </p>
                             <p>
-                                In this 3 months I had time to do 2 things, the first one is to migrate my website again
-                                to <a
+                                Despite all inconvenience, in this 3 months I had time to do 2 things, the first one is
+                                to migrate my website again to <a
                                 href='https://webpack.github.io/'>Webpack</a> and <a
-                                href='https://facebook.github.io/react/'>React</a>/<a
-                                href='https://github.com/rackt/redux'>Redux</a>, and then finally to Jspm,
-                                and to develop a NPM module called <a
-                                href='https://elephantseed.firebaseapp.com/'>Slush-jspm-react-seed</a>.
+                                href='https://facebook.github.io/react/'>React</a> / <a
+                                href='https://github.com/rackt/redux'>Redux</a>, but also I developed a NPM
+                                module called <a href='https://elephantseed.firebaseapp.com/'>Slush-Jspm-React-Seed</a>.
                             </p>
                             <p>
-                                <a
-                                    href='https://webpack.github.io/'>Webpack</a> it's gorgeous, with minimum
-                                configuration you get a lot of goodies out of the
-                                box.
-                                this website has been rebuild 3 times, first was Angruntlar, then Angulpar, and now it's
-                                called Slush-React-Redux.
-                            </p>
-                            <p>
+                                <a href='http://jspm.io/'>JSPM</a> it's gorgeous, you get a lot of goodies out of
+                                the box practically doing nothing.
+                                This website has been rebuilt 3 times, first was <a
+                                href='https://github.com/sloppylopez/angruntlar'>Angruntlar</a>, then <a
+                                href='https://github.com/sloppylopez/angruntlar'>Angulpar</a>, and now it's
+                                called <a href='https://github.com/sloppylopez/slush-react-redux'>Slush-React-Redux</a>.
+                                In the end I decided to use <a href='http://jspm.io/'>JSPM</a> with <a
+                                href='http://jspm.io/'>Babel</a> to have ES6 Sugar and minimum configuration.
                                 This version has practically the same features as the other 2 but with much less files
-                                and much less lines of code, and even though I have not measured it yet, I'm sure the
-                                performance in better in practically every aspect.
+                                and fewer lines of code, and even though I have not measured it yet, the
+                                performance in better in practically every aspect surely.
                             </p>
                             <p>
-                                The second project it's something I got into when speaking with one of the developers
-                                of the new team, he showed me JSPM and I got fascinated by it, I would say it's not
-                                production ready yet, but it tries to solve one of my major complaints about Javascript
-                                in the first article I wrote here, it tries to unify the module/dependency management
-                                of your project, and your builds, and your bundling, everything under the same tool.
+                                The second project it's called <a href='https://elephantseed.firebaseapp.com/'>Elephant Seed</a>, I got into it after speaking with one of the developers
+                                of my team, he showed me JSPM and I got interested, it tries to solve one of my major complaints about Javascript
+                                in the first article I wrote here, unifying the module dependency management
+                                of your project, and your builds, and your bundling everything under the same roof.
                             </p>
                             <p>
                                 In theory, with JSPM you don't need Gulp, that's when I decided to make a front-end
-                                project without Gulp, just to see if it's possible to get the same goodies but without
+                                project without Gulp, just to see if it's possible to get the same goodies without
                                 it.
                             </p>
                             <p>
-                                I was also very interested in seeds and build tools like Yeoman, so my colleague showed
-                                me Slush with is like Yeoman but based on Gulp, which is used transparently for me, it's
-                                just to build the seed after that there is no more Gulp involved.
+                                My colleague also showed me Slush, it's a build tool like Yeoman based on Gulp, so I decided
+                                to create a seed for removing the boiler-plate of a front-end project using all this technologies
+                                and including all the features developers like, for example live reloading, bundling, minifying
+                                and so forth, all without the need to depend on Gulp and 20 different plug-ins.
                             </p>
                             <p>
-                                This technology it's very recent, so I am encountering some problems in production mode,
-                                I
-                                am waiting for a release in January to see If I can advance, If not maybe I will have to
-                                surrender, and start using Gulp for few tasks, or Monkey Patch.<br></br>
-                                I can't wait to see how it ends.
+                                In the end you want to develop your project with the minimum ammount of dependencies but maintianing certains
+                                features that are very usefull and time saving for dev's in the development phase and even on production release.
                             </p>
                             <footer><p className='pink'><b>ڪ.</b></p></footer>
                         </div>
@@ -147,7 +141,7 @@ class MainBody extends Component {
                                 You run terrified towards your desk to find that <a
                                 href='https://www.jetbrains.com/idea/'>Intellij</a> now is called <a
                                 href='https://www.jetbrains.com/webstorm/'>Webstorm</a>,
-                                <a href='http://www.apache.org/'>Apache</a> is <a
+                                <a href='http://www.apache.org/'> Apache</a> is <a
                                 href='https://www.nginx.com/'>Nginx</a> and <a
                                 href='http://tomcat.apache.org/'>Apache Tomcat</a> is known by the name of <a
                                 href='https://nodejs.org/en/'>NodeJS</a>,
@@ -172,8 +166,7 @@ class MainBody extends Component {
                                 things like for example that I can do the same as before! Yuhuuu, what else?.
                             </p>
                             <p>
-                                Asynchrony 4 all!, Ok, <a
-                                href='https://java.com/en/download/'>Java</a> multi-threading complexity problem solved,
+                                Asynchrony 4 all!, Ok, efficiency with concurrency that's nice,
                                 what else?.<br></br>
                                 Ludicrous speed builds, niceee (Problem solved in <a
                                 href='https://java.com/en/download/'>Java</a> today with <a
@@ -210,7 +203,7 @@ class MainBody extends Component {
                                 between:
                             </p>
                             <br></br>
-                            <p>(naming few of them)</p>
+                            <p>Naming few of them</p>
                             <ul>
                                 <li>
                                     <a
@@ -273,7 +266,7 @@ class MainBody extends Component {
                                 <li>
                                     <a
                                         href='http://requirejs.org/'>RequireJS </a>
-                                     vs <a
+                                    vs <a
                                     href='https://webpack.github.io/'>Webpack</a> vs
                                     <a
                                         href='http://browserify.org/'> Browserify</a> vs <a
@@ -328,7 +321,7 @@ class MainBody extends Component {
                                 and easy to build following the tutorial, but soon I though maybe we could do something
                                 interesting with the
                                 <a
-                                    href='https://www.elastic.co/'>Elastic Search</a>.<br></br>
+                                    href='https://www.elastic.co/'> Elastic Search</a>.<br></br>
                                 So taking some random but useful data, in this case RSS news to persist on the Elastic
                                 Search, gave me the
                                 need
@@ -358,8 +351,8 @@ class MainBody extends Component {
                                 must have a CI-CD
                                 cycle, it was very easy to achieve using the
                                 wonders of <a href='https://travis-ci.org/sloppylopez/angulpar'>Travis</a> mixed up with
-                                <a href='https://www.firebase.com/'>Firebase</a> <a
-                                href='http://en.wikipedia.org/wiki/Mobile_Backend_as_a_service'>BaaS</a>
+                                <a href='https://www.firebase.com/'> Firebase</a> <a
+                                href='http://en.wikipedia.org/wiki/Mobile_Backend_as_a_service'> BaaS </a>
                                 features.<br></br>
                                 If we include <a href='https://github.com/sloppylopez'>Github</a> in the process, we
                                 have
